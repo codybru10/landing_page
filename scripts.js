76b5e325bf43a813dd61a2b73aaaa7317071d052
Cody <cody.brubaker@gmail.com>
2017-05-05 11:06:31 -0400

    add scrollspy

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 $( document ).ready(function() {
   $('.well').click(function() {
-    
+
     alert( "ready!" );
   });
+
+  $('body').scrollspy({ target: '#about' })
+  $('body').scrollspy({ target: '#projects' })
+  $('body').scrollspy({ target: '#skills' })
+  $('body').scrollspy({ target: '#contact' })
 });
